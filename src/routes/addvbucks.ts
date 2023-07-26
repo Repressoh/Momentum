19eac3eddffe66a7b19206f1c2685b6d2a5a285e
Repressoh <repressoh@gmail.com>
2023-07-26 18:14:29 +0200

    Vbucks api

---
+++ src/routes/addvbucks.ts
@@ -0,0 +1,78 @@
+import express from "express";
+import path, { parse } from "path";
+const app = express.Router();
+import { dirname } from 'dirname-filename-esm';
+const __dirname = dirname(import.meta);
+import mongoose from 'mongoose';
+import axios from 'axios';
+import fs from 'node:fs';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+
+import Profile from '../model/profiles.js';
+import User from '../model/user.js';
+console.log("Loaded addvbucks.ts");
+app.post("/addvbucks", async (req, res) => {
+    const { authkey, username, addValue } = req.query;
+  
+    if (!username) return res.status(400).send('No username provided.');
+    if (!addValue) return res.status(400).send('No addValue provided.');
+    if (!authkey) return res.status(400).send('No authkey provided.');
+
+    const lowerUsername = (username as string).toLowerCase();
+  
+    if (authkey === process.env.authkey) {
+      try {
+        const user = await User.findOne({ username_lower: lowerUsername });
+
+        if (user) {
+          const filter = { accountId: user.accountId };
+          const update = { $inc: { 'profiles.common_core.items.Currency:MtxPurchased.quantity': parseInt(addValue as string) } };
+          const options = { new: true };
+  
+          const updatedProfile = await Profile.findOneAndUpdate(filter, update, options);
+  
+          if (updatedProfile) {
+            const newQuantity = updatedProfile.profiles.common_core.items['Currency:MtxPurchased'].quantity;
+            return res.status(200).json({ quantity: newQuantity });
+          } else {
+            return res.status(404).send('Profile not found or item not found.');
+          }
+        } else {
+          return res.status(404).send('User not found.');
+        }
+      } catch (err) {
+        console.error('Error while updating data:', err);
+        return res.status(500).send('Error while updating data.');
+      }
+    } else {
+      const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+      const date = new Date().toLocaleDateString();
+      const time = new Date().toLocaleTimeString();
+      const embeds = [
+        {
+          title: "Unauthorized API Request",
+          color: 15548997,
+          description: "Unauthorized API request from: " + ip + "\nAt: " + date + " " + time + "\nWith key: " + authkey + "\nFor user: " + username + "\nWith value: " + addValue + ".",
+        },
+      ];
+  
+      const data = JSON.stringify({ embeds });
+      const config_axios = {
+        method: "POST",
+        url: process.env.webhook_url,
+        headers: { "Content-Type": "application/json" },
+        data: data,
+      };
+  
+      axios(config_axios)
+        .catch((error) => {
+          console.error('Error sending Discord webhook:', error.message);
+        });
+  
+      return res.status(401).send('Unauthorized');
+    }
+  });
+
+  export default app;
